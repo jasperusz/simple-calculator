7cde4e2bd429b8da562d21ef02244de2e4cb3d46
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-04-02 22:04:35 -0300

    add function to get active operator

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let activeButton = 'clear';
 let initialValue = 0;
-let numberPressed = '0';
+let buttonPressed = '0';
 let consoleAcumulator = '';
 let consoleText = document.getElementById('consoleText');
 const getNumberButtons = document.getElementsByClassName('numberButtons');
 const maxNumbers = 9;
 
 function getButtonNumber(buttonClick) {
-    numberPressed = buttonClick;
-    consoleAcumulator += numberPressed;
+    buttonPressed = buttonClick;
+    consoleAcumulator += buttonPressed;
     consoleText.textContent = `${consoleAcumulator}`;
     if (consoleAcumulator.length == maxNumbers) {
         for (let buttons of getNumberButtons) {
@@ -21,11 +21,20 @@ function getButtonNumber(buttonClick) {
 // Function to Clear the Calculator when the C button is pressed //
 function clearCalculator(buttonClick) {
     if (buttonClick === 'clear') {
+        initialValue = 0;
         consoleAcumulator = '';
-        numberPressed = '0';
+        buttonPressed = '0';
         document.getElementById('consoleText').textContent = '0';
         for (let buttons of getNumberButtons) {
             buttons.disabled = false;
         };
     };
+};
+
+// Function to get active operator and acumulate the first numbers //
+function getActiveOperator(buttonClick) {
+    initialValue = parseInt(consoleAcumulator);
+    consoleText.textContent = buttonClick;
+    consoleAcumulator = '';
+    activeButton = buttonClick;
 };

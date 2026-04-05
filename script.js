bb99f133d3026a18d7eb718becf617a5b74873f7
Jasper <235980385+jasperusz@users.noreply.github.com>
2026-04-05 20:46:51 -0300

    add fix when equal is pressed and second number is empty

--- script.js
+++ script.js
@@ -25,7 +25,8 @@ function clearCalculator(buttonClick) {
         initialValue = 0;
         consoleAcumulator = '';
         buttonPressed = '0';
-        document.getElementById('consoleText').textContent = '0';
+        activeButton = 'clear';
+        document.getElementById('consoleText').textContent = 'CLEAR';
         for (let buttons of getNumberButtons) {
             buttons.disabled = false;
         };
@@ -34,6 +35,9 @@ function clearCalculator(buttonClick) {
 
 // Function to get active operator and acumulate the first numbers //
 function getActiveOperator(buttonClick) {
+    if (consoleAcumulator === '') {
+        return;
+    };
     if (activeButton !== 'clear') {
         operate(buttonClick); 
     };
@@ -47,6 +51,7 @@ function getActiveOperator(buttonClick) {
 // Operate function which get the activebutton and activate within pressing result button //
 function operate(buttonClick) {
     buttonPressed = '=';
+            // It only shows the result when resultButton is pressed
     if (activeButton === '+') {
         consoleAcumulator = initialValue + parseInt(consoleAcumulator);
         } else if (activeButton === '-') {
@@ -60,9 +65,15 @@ function operate(buttonClick) {
             consoleAcumulator = initialValue / parseInt(consoleAcumulator);
             };
         };
-        // It only shows the result when resultButton is pressed
-        if (buttonClick === '=') {
+        if (buttonClick === '=' && consoleAcumulator !== '') {
+        consoleText.textContent = `${consoleAcumulator}`
+        } else if (buttonClick === '=' && consoleAcumulator === '') {
+            consoleAcumulator = initialValue;
+            consoleText.textContent = `${consoleAcumulator}` 
+        };
+        if (buttonClick === '=' && Number.isNaN(consoleAcumulator) === true) {
+            consoleAcumulator = 0;
             consoleText.textContent = `${consoleAcumulator}`
-        };  
+        };
     };
 
